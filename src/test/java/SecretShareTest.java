0145a21883feac54cb1bbf19433b69e3689e947c
alebian <alebezdjian@gmail.com>
2017-06-21 23:02:16 -0300

    Renamed shadows in test

--- src/test/java/SecretShareTest.java
+++ src/test/java/SecretShareTest.java
@@ -18,6 +18,17 @@ public class SecretShareTest {
       1, 2, 3, 4, 5, 6, 7, 8,
   };
 
+  int[][] shadows = {
+      { 36,  36,  36,  36,  36,  36,  36,  36},
+      {251, 251, 251, 251, 251, 251, 251, 251},
+      {189, 189, 189, 189, 189, 189, 189, 189},
+      {174, 174, 174, 174, 174, 174, 174, 174},
+      {228, 228, 228, 228, 228, 228, 228, 228},
+      { 86,  86,  86,  86,  86,  86,  86,  86},
+      { 23,  23,  23,  23,  23,  23,  23,  23},
+      {151, 151, 151, 151, 151, 151, 151, 151}
+  };
+
   @Test
   public void randomizeTest() {
     int[] test = SecretShare.randomize(image, table);
@@ -29,24 +40,14 @@ public class SecretShareTest {
   @Test
   public void generateShadows() {
     int[][] test = SecretShare.generateShadows(8, 8, randomizedImage);
-    int[][] expected = {
-        { 36,  36,  36,  36,  36,  36,  36,  36},
-        {251, 251, 251, 251, 251, 251, 251, 251},
-        {189, 189, 189, 189, 189, 189, 189, 189},
-        {174, 174, 174, 174, 174, 174, 174, 174},
-        {228, 228, 228, 228, 228, 228, 228, 228},
-        { 86,  86,  86,  86,  86,  86,  86,  86},
-        { 23,  23,  23,  23,  23,  23,  23,  23},
-        {151, 151, 151, 151, 151, 151, 151, 151}
-    };
 
-    assertArrayEquals(expected[0], test[0]);
-    assertArrayEquals(expected[1], test[1]);
-    assertArrayEquals(expected[2], test[2]);
-    assertArrayEquals(expected[3], test[3]);
-    assertArrayEquals(expected[4], test[4]);
-    assertArrayEquals(expected[5], test[5]);
-    assertArrayEquals(expected[6], test[6]);
-    assertArrayEquals(expected[7], test[7]);
+    assertArrayEquals(shadows[0], test[0]);
+    assertArrayEquals(shadows[1], test[1]);
+    assertArrayEquals(shadows[2], test[2]);
+    assertArrayEquals(shadows[3], test[3]);
+    assertArrayEquals(shadows[4], test[4]);
+    assertArrayEquals(shadows[5], test[5]);
+    assertArrayEquals(shadows[6], test[6]);
+    assertArrayEquals(shadows[7], test[7]);
   }
 }
